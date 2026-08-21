6fc73fdf1ae9b801a74b5b92b50eaec4ab95e437
Ashish Pandey <ap965599@gmail.com>
2026-08-22 02:39:31 +0530

    Make v0.2.0 visibly faster for OTA verification

--- version.py
+++ version.py
@@ -7,8 +7,11 @@ if not re.fullmatch(r'\d+\.\d+\.\d+', version):
     version = '0.1.0'
 
 build_id = os.getenv('FW_BUILD_ID', f'led-blink-central-test::{version}')
+major, minor, patch = [int(part) for part in version.split('.')]
+blink_interval = 250 if (major, minor, patch) >= (0, 2, 0) else 1000
 
 env.Append(CPPDEFINES=[
     ('UEC_FIRMWARE_VERSION', env.StringifyMacro(version)),
     ('UEC_BUILD_ID', env.StringifyMacro(build_id)),
+    ('UEC_BLINK_INTERVAL_MS', str(blink_interval)),
 ])
