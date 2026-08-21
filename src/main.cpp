d6ecf99df917940be5e0a829cfc7c0df6b8ed8a6
Ashish Pandey <ap965599@gmail.com>
2026-08-22 02:34:03 +0530

    Make LED test diagnostics explicit

--- src/main.cpp
+++ src/main.cpp
@@ -4,29 +4,23 @@
 #ifndef UEC_CONTROLLER_URL
 #define UEC_CONTROLLER_URL "https://esp32-universal-controller.onrender.com"
 #endif
-
 #ifndef UEC_FIRMWARE_VERSION
 #define UEC_FIRMWARE_VERSION "0.1.0"
 #endif
-
 #ifndef UEC_BUILD_ID
 #define UEC_BUILD_ID "led-blink-central-test::0.1.0"
 #endif
 
-// The generic ESP32 Dev Module target does not define LED_BUILTIN.
-// GPIO2 is the onboard LED pin on the common ESP32 DevKit boards.
 constexpr uint8_t LED_PIN = 2;
 constexpr unsigned long BLINK_INTERVAL_MS = 1000;
 
 ControllerClient controller(UEC_CONTROLLER_URL, UEC_FIRMWARE_VERSION, UEC_BUILD_ID);
-
 bool ledState = false;
 unsigned long lastBlinkAt = 0;
 
 void setup() {
   Serial.begin(115200);
   delay(500);
-
   pinMode(LED_PIN, OUTPUT);
   digitalWrite(LED_PIN, LOW);
 
@@ -34,7 +28,8 @@ void setup() {
   Serial.println("========================================");
   Serial.println(" ESP32 LED Blink - Central Test Project");
   Serial.println("========================================");
-
+  Serial.printf("[FW] Version: %s | Build: %s | Hardware: esp32\n", UEC_FIRMWARE_VERSION, UEC_BUILD_ID);
+  Serial.printf("[APP] LED GPIO: %u | Blink interval: %lu ms\n", LED_PIN, BLINK_INTERVAL_MS);
   controller.begin();
 }
 
@@ -46,7 +41,7 @@ void loop() {
     lastBlinkAt = now;
     ledState = !ledState;
     digitalWrite(LED_PIN, ledState ? HIGH : LOW);
-    Serial.printf("[LED] %s\n", ledState ? "ON" : "OFF");
+    Serial.printf("[LED] GPIO %u -> %s\n", LED_PIN, ledState ? "ON" : "OFF");
   }
 
   String remoteMessage;
