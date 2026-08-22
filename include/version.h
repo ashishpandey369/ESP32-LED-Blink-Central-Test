ed6adf1f61fc3414f0653e91df5306565a9a6deb
Ashish Pandey <ap965599@gmail.com>
2026-08-22 22:15:37 +0530

    Release v0.4.0

--- include/version.h
+++ include/version.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define UEC_FIRMWARE_VERSION "0.1.0"
-#define UEC_BUILD_ID "led-blink-central-test::0.1.0"
+#define UEC_FIRMWARE_VERSION "0.4.0"
+#define UEC_BUILD_ID "led-blink-central-test::0.4.0"
 #define UEC_PROJECT_ID "led-blink-central-test"
 #define UEC_HARDWARE_TARGET "esp32"
