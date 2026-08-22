57c56b2b79ce6ebe1019463bacc93504099deeae
Ashish Pandey <ap965599@gmail.com>
2026-08-23 01:17:49 +0530

    Improve OTA stream read tolerance

--- src/controller_client.cpp
+++ src/controller_client.cpp
@@ -24,7 +24,7 @@ constexpr unsigned long FIRST_HEARTBEAT_RETRY_MS = 5000;
 constexpr unsigned long WIFI_RETRY_INTERVAL_MS = 10000;
 constexpr unsigned long NTP_SYNC_TIMEOUT_MS = 10000;
 constexpr unsigned long OTA_NO_PROGRESS_TIMEOUT_MS = 60000;
-constexpr unsigned long OTA_STREAM_READ_TIMEOUT_MS = 1000;
+constexpr unsigned long OTA_STREAM_READ_TIMEOUT_MS = 5000;
 DNSServer dnsServer;
 WebServer webServer(HTTP_PORT);
 Preferences preferences;
